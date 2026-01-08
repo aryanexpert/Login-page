f86f6d24873863734f0e6730156f88bf593616fe
aryanexpert <aryankanki11@gmail.com>
2026-01-08 22:46:00 +0530

    Update authController.js

--- config/models/controllers/authController.js
+++ config/models/controllers/authController.js
@@ -1,1 +1,42 @@
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+// REGISTER
+exports.register = async (req, res) => {
+  const { email, password } = req.body;
+
+  const userExists = await User.findOne({ email });
+  if (userExists) return res.status(400).json({ msg: "User already exists" });
+
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  const user = await User.create({ email, password: hashedPassword });
+
+  res.json({ msg: "User registered successfully" });
+};
+
+// LOGIN
+exports.login = async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+  if (!user) return res.status(400).json({ msg: "Invalid credentials" });
+
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) return res.status(400).json({ msg: "Invalid credentials" });
+
+  const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+    expiresIn: "1d"
+  });
+
+  res.json({ token });
+};
+
+// GET ALL USERS (ADMIN)
+exports.getUsers = async (req, res) => {
+  const users = await User.find().select("-password");
+  res.json(users);
+};
 
